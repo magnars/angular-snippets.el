90f44d0b624d761596b786ccc299c86eff970249
Magnar Sveen <magnars@gmail.com>
2013-05-03 22:42:29 +0200

    Release 0.2.1

--- angular-snippets.el
+++ angular-snippets.el
@@ -4,7 +4,7 @@
 
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;; Keywords: snippets
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((s "1.4.0") (dash "1.2.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
